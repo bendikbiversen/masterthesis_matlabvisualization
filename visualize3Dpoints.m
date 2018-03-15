87bedc7a9c29c443191083c221f2902ad6a982b4
Bendik Iversen, desktop PC, Ubuntu 14.04 <bendikbi@stud.ntnu.no>
2018-03-15 16:18:55 +0100

    Add plotting of OpenCV-3D-points

--- visualize3Dpoints.m
+++ visualize3Dpoints.m
@@ -2,17 +2,18 @@ clear all
 clc
 
 % Filling M 3D points from file into a Mx3 array
-fileID = fopen('points3D.txt', 'r');
-[A, count] = fscanf(fileID, '%f');
+% 3D points from DUO SDK
+fileID = fopen('frame_t1_3D.txt', 'r');
+[A, count_sdk] = fscanf(fileID, '%f');
 fclose(fileID);
 
-numPoints = count/3;
-points3d = zeros(numPoints, 3);
+numPoints_sdk = count_sdk/3;
+points3d_sdk = zeros(numPoints_sdk, 3);
 
 n = 1;
 m = 1;
-for i = 1:count
-    points3d(m, n) = A(i);
+for i = 1:count_sdk
+    points3d_sdk(m, n) = A(i);
     n = n+1;
     if mod(i, 3) == 0
        n = 1; 
@@ -20,10 +21,33 @@ for i = 1:count
     end
 end
 
-pcshow(points3d);
+figure(1)
+pcshow(points3d_sdk);
 
-X = points3d(:, 1);
-Y = points3d(:, 2);
-Z = points3d(:, 3);
+% 3D points from OpenCV, cv::reprojectImageTo3D
+fileID = fopen('frame_t1_3D_opencv.txt', 'r');
+[B, count_cv] = fscanf(fileID, '%f');
+fclose(fileID);
 
-scatter(X, Y, Z);
+numPoints_cv = count_cv/3;
+points3d_cv = zeros(numPoints_cv, 3);
+
+n = 1;
+m = 1;
+for i = 1:count_cv
+    points3d_cv(m, n) = B(i);
+    n = n+1;
+    if mod(i, 3) == 0
+       n = 1; 
+       m = m+1;
+    end
+end
+
+figure(2)
+pcshow(points3d_cv);
+
+%X = points3d(:, 1);
+%Y = points3d(:, 2);
+%Z = points3d(:, 3);
+
+%scatter(X, Y, Z);
